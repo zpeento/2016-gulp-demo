d2e011ca8ca66797973c628a34e2e5fa5b250a10
iceiceice <509887158@qq.com>
2016-12-06 19:52:49 +0800

    [update]通过gulp-less编译less文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+// gulp.task('my task 1',function () {
+// 	console.log('task 1');
+// })
 
-gulp.task('my task 1',function () {
-	console.log('task 1');
-})
-
-gulp.task('my task 2',function () {
-	console.log('task 2');
-})
+// gulp.task('my task 2',function () {
+// 	console.log('task 2');
+// })
 
 
 //运行gulp会执行默认任务
 //对任务顺序进行排序，默认的任务在最后执行
-gulp.task('default',['my task 1','my task 2'],function () {
-	console.log('hello gulp!')
+// gulp.task('default',['my task 1','my task 2'],function () {
+// 	console.log('hello gulp!')
+// });
+
+gulp.task('less',function () {
+	return gulp.src('./public/stylesheets/less/*.less')
+	.pipe(less())
+	.pipe(gulp.dest('./public/stylesheets/css'))
 });
+
+gulp.task('default',['less'],function () {
+	console.log('success')
+})
