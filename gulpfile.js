bcfd9831d59d624a6dff5d58469e62561b2dbf1f
iceiceice <509887158@qq.com>
2016-12-06 16:48:39 +0800

    [update]多任务，对任务进行排序

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
-//运行gulp会执行默认任务
-gulp.task('default',function(){
-	console.log('hello gulp!')
+
+gulp.task('my task 1',function () {
+	console.log('task 1');
 })
+
+gulp.task('my task 2',function () {
+	console.log('task 2');
+})
+
+
+//运行gulp会执行默认任务
+//对任务顺序进行排序，默认的任务在最后执行
+gulp.task('default',['my task 1','my task 2'],function () {
+	console.log('hello gulp!')
+});
